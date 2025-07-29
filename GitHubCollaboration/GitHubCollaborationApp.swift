72f41077efab975bb3b9b2232d23d7d5c9b0c87f
Vachi Kalra <vachikalra@gmail.com>
2025-07-29 10:10:44 -0700

    Initial Commit

---
+++ GitHubCollaboration/GitHubCollaborationApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubCollaborationApp.swift
+//  GitHubCollaboration
+//
+//  Created by Vachi Kalra on 7/29/25.
+//
+
+import SwiftUI
+
+@main
+struct GitHubCollaborationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GitHubCollaborationTests/GitHubCollaborationTests.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubCollaborationTests.swift
+//  GitHubCollaborationTests
+//
+//  Created by Vachi Kalra on 7/29/25.
+//
+
+import Testing
+@testable import GitHubCollaboration
+
+struct GitHubCollaborationTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
